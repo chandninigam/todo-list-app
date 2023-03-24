94e8619b61c8fde8cbf8350625c1423ddca1c780
chandni nigam <nigamchandni139@gmail.com>
2023-03-24 16:03:56 +0530

    Refactor file and code

---
+++ src/components/Complete.js
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+
+const { height } = Dimensions.get("window");
+
+export function Completed() {
+  return (
+    <View style={completeTabStyle.wrapper}>
+      <View style={completeTabStyle.header}>
+        <Text style={completeTabStyle.headingText}>Completed</Text>
+      </View>
+    </View>
+  );
+}
+
+const completeTabStyle = StyleSheet.create({
+  header: {
+    display: "flex",
+    marginTop: height / 16,
+    padding: height / 80,
+  },
+  headingText: {
+    display: "flex",
+    alignSelf: "flex-start",
+    fontSize: height / 32,
+    fontWeight: "700",
+  },
+});
